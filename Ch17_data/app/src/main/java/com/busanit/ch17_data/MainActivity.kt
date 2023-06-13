620a00b8cbf9909b92a26fe1c7552823546bef9d
enoch012 <zzan6238@naver.com>
2023-06-13 15:17:49 +0900

    0613 / DB 연동하여 데이터 등록 및 출력 방법 2가지 - DBHelper 클래스 생성(테이블 생성) - insert 함수 사용 - query 함수 사용

--- Ch17_data/app/src/main/java/com/busanit/ch17_data/MainActivity.kt
+++ Ch17_data/app/src/main/java/com/busanit/ch17_data/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.busanit.ch17_data
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,16 +25,22 @@ class MainActivity : AppCompatActivity() {
     val textView = binding.textView
 
     // 1. openOrCreateDatabase 활용
-    val db = openOrCreateDatabase("test.db", Context.MODE_PRIVATE, null)
+    /*val db = openOrCreateDatabase("test.db", Context.MODE_PRIVATE, null)
 
     // 테이블 생성
     db.execSQL("create table if not exists USER_TB (" +
         "_id integer primary Key autoincrement, " +
         "name text not null, " +
-        "phone text)")
+        "phone text)")*/
 
-    // 입력
-    btnInsert.setOnClickListener {
+    // 2. SQLiteOpenHelper 클래스 사용
+    val db: SQLiteDatabase = DBHelper(this).writableDatabase
+
+    // writableDatabase : 입력, 수정, 삭제
+    // readableDatabase : 조회
+
+    // 입력 1
+    /*btnInsert.setOnClickListener {
       db.execSQL("insert into USER_TB(name, phone) values (?, ?)",
       arrayOf<String>(editName.text.toString(), editTel.text.toString())
       )
@@ -40,10 +48,21 @@ class MainActivity : AppCompatActivity() {
       Toast.makeText(this, "입력되었습니다.", Toast.LENGTH_SHORT).show()
       editName.text.clear()
       editTel.text.clear()
+    }*/
+
+    // 입력 2. insert() 함수 사용
+    btnInsert.setOnClickListener {
+      val values = ContentValues()
+      values.put("name", editName.text.toString())
+      values.put("phone", editTel.text.toString())
+      db.insert("USER_TB", null, values)
+      Toast.makeText(this, "입력되었습니다.", Toast.LENGTH_SHORT).show()
+      editName.text.clear()
+      editTel.text.clear()
     }
 
-    // 조회
-    btnSelect.setOnClickListener {
+    // 조회 1
+    /*btnSelect.setOnClickListener {
       val cursor = db.rawQuery("select * from USER_TB", null)
       val str = StringBuilder()
       if(cursor != null){
@@ -57,7 +76,29 @@ class MainActivity : AppCompatActivity() {
 
         textView.text = str
       }
+    }*/
+
+    // 조회 2.query() 함수 사용
+    // 커서를 받아오는 방법이 조금 다르다
+
+    btnSelect.setOnClickListener {
+      val cursor = db.query("USER_TB", arrayOf("_id", "name", "phone"), null, null, null, null, null)
+
+      val str = StringBuilder()
+      if(cursor != null){
+        while (cursor.moveToNext()){ // hasNext와같은 것
+          val id = cursor.getInt(0)
+          val name = cursor.getString(1)
+          val phone = cursor.getString(2)
+          Log.d("myLog", "namd : $name, phone : $phone")
+          str.append("$id. name : $name, phone : $phone \n")
+        }
+
+        textView.text = str
+      }
     }
+
+
 
   }
 }
